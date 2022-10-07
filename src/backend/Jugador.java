25918504b2ef13f2f0eb0b1d5629b964a5c73f7b
Alexander Mamani Yucra <akeymy4@gmail.com>
2022-10-07 12:52:35 -0400

    n

--- src/backend/Jugador.java
+++ src/backend/Jugador.java
@@ -9,7 +9,7 @@ public class Jugador implements Comparable<Jugador>{
         puntaje = 0;
     }
 
-    public Juga dor(String nick, int puntaje) {
+    public Jugador(String nick, int puntaje) {
         this.nick = nick;
         this.puntaje = puntaje;
     }
